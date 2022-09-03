cea6b289981d138fbcbc60f066ef4a1f960f1279
CarlosAlvarado-git <61555440+CarlosAlvarado-git@users.noreply.github.com>
2022-09-03 13:39:08 -0600

    ya tengo los clientes

--- Laboratorio_4/lab4.R
+++ Laboratorio_4/lab4.R
@@ -2,78 +2,94 @@ library(readr)
 library(dplyr)
 library(stringr)
 df <- read.csv('tabla_completa.csv', sep = ',')
+df1 <- read.csv('tabla_completa_v1.csv', sep = ',')
+df1 <- select(df1, -X.1, -X.2, -X.3)
 
-
-# Adding column based on other column:
 #Marcar faltantes
 df <- df %>%
-  mutate(Faltante = case_when(
+  mutate(faltante = case_when(
     grepl("faltante", tolower(df$CLIENTE)) ~ 1,
     
   ))
 #Marcar despacho al cliente
 df <- df %>%
   mutate(despacho_cliente = case_when(
-    grepl("despacho a cliente",tolower(df$CLIENTE)) ~ 2
+    grepl("despacho a cliente",tolower(df$CLIENTE)) ~ 1
   ))
 #Marcar devolucion
 df <- df %>%
   mutate(devolucion = case_when(
-    grepl("devolucion",tolower(df$CLIENTE)) ~ 3
+    grepl("devolucion",tolower(df$CLIENTE)) ~ 1
   ))
+df1 <- df1 %>% mutate(faltante = df$faltante)
+df1 <- df1 %>% mutate(despacho_cliente = df$despacho_cliente)
+df1 <- df1 %>% mutate(devolucion = df$devolucion)
+df <- df1
+df$CLIENTE <- iconv(df$CLIENTE, to = 'UTF-8')
+df$CLIENTE <- str_remove_all(df$CLIENTE, " ")
 
 
 
 #Ver cuentos faltantes hay 
 df %>%
-  select(Faltante) %>%
-  filter(Faltante == 1) %>%
-  summarise(n = n())
+  select(faltante, despacho_cliente, devolucion) %>%
+  filter(faltante == 1) %>%
+  filter(is.na(despacho_cliente)) %>%
+  filter(is.na(devolucion)) %>%
+  summarise(faltantes = n())
 #ver despacho a cliente
 df %>%
-  select(despacho_cliente) %>%
-  filter(despacho_cliente == 2) %>%
-  summarise(n = n())
+  select(faltante, despacho_cliente, devolucion) %>%
+  filter(despacho_cliente == 1) %>%
+  filter(is.na(faltante)) %>%
+  filter(is.na(devolucion)) %>%
+  summarise(despacho_clientes = n())
 #ver devolucion
 df %>%
-  select(devolucion) %>%
-  filter(devolucion == 3) %>%
-  summarise(n = n())
+  select(faltante, despacho_cliente, devolucion) %>%
+  filter(devolucion == 1) %>%
+  filter(is.na(despacho_cliente)) %>%
+  filter(is.na(faltante)) %>%
+  summarise(devoluciones = n())
+
+
+
 # Ver cuales son faltantes que fueron al despacho al cliente
 df %>%
-  select(Faltante, despacho_cliente) %>%
-  filter(Faltante == 1) %>%
-  filter(despacho_cliente == 2) %>%
+  select(faltante, despacho_cliente) %>%
+  filter(faltante == 1) %>%
+  filter(despacho_cliente == 1) %>%
   summarise(faltante_despacho_a_cliente = n())
-# Ver cuales son faltantes que fueron al despacho al cliente y devolucion
-df %>%
-  select(Faltante, despacho_cliente, devolucion) %>%
-  filter(Faltante == 1) %>%
-  filter(despacho_cliente == 2) %>%
-  filter(devolucion == 3) %>%
-  summarise(fal_despa_devolu = n())
+
+
 # sin clasificacion/ extra
 df %>%
-  select(Faltante, despacho_cliente, devolucion) %>%
-  filter(is.na(Faltante)) %>%
+  select(faltante, despacho_cliente, devolucion) %>%
+  filter(is.na(faltante)) %>%
   filter(is.na(despacho_cliente)) %>%
   filter(is.na(devolucion)) %>%
   summarise(sincat = n())
 
 
+
 # ver la cantidad de cada credito, teniendo un envio de faltante
 df %>%
-  select(CREDITO, Faltante) %>%
-  filter(Faltante == 1) %>%
+  select(CREDITO, faltante, despacho_cliente, devolucion) %>%
+  filter(faltante == 1) %>%
+  filter(is.na(despacho_cliente)) %>%
+  filter(is.na(devolucion)) %>%
   group_by(CREDITO) %>%
   summarise(cant = n())
-# Ver cuales son faltantes que fueron al despacho al cliente
+# Ver cuales son faltantes que fueron al despacho al cliente, con crédito
 df %>%
-  select(Faltante, despacho_cliente, CREDITO) %>%
-  filter(Faltante == 1) %>%
-  filter(despacho_cliente == 2) %>%
+  select(faltante, despacho_cliente, CREDITO) %>%
+  filter(faltante == 1) %>%
+  filter(despacho_cliente == 1) %>%
   group_by(CREDITO) %>%
   summarise(cant = n())
+
+
+
 #Viajes por mes
 df %>%
   select(MES) %>%
@@ -88,7 +104,28 @@ df %>%
 
 #Transporte con faltante
 df %>%
-  select(UNIDAD, Faltante) %>%
-  filter(Faltante == 1) %>%
+  select(UNIDAD, faltante, CANTIDAD, despacho_cliente, devolucion) %>%
+  filter(faltante == 1) %>%
+  filter(is.na(despacho_cliente)) %>%
+  filter(is.na(devolucion)) %>%
   group_by(UNIDAD) %>%
-  summarise(cant = n())
+  summarise(cant_viajes = n(),can_unidades = sum(CANTIDAD)) 
+
+#CLIENTES VIAJES
+df %>%
+  select(CLIENTE) %>%
+  group_by(CLIENTE) %>%
+  summarise(pedidos = n()) %>%
+  arrange(desc(pedidos))
+
+#clientes con faltante
+df %>%
+  select(CLIENTE, faltante, despacho_cliente, devolucion) %>%
+  filter(faltante == 1) %>%
+  filter(is.na(despacho_cliente)) %>%
+  filter(is.na(devolucion)) %>%
+  group_by(CLIENTE) %>%
+  summarise(pedidos = n(), faltantes = sum(faltante)) 
+
+
+
