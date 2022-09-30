2b21dbaad67256bd536ff169314d541c31acb407
CarlosAlvarado-git <61555440+CarlosAlvarado-git@users.noreply.github.com>
2022-09-29 18:17:28 -0600

    Update Laboratorio 5.R

--- Laboratorio5/Laboratorio 5.R
+++ Laboratorio5/Laboratorio 5.R
@@ -9,6 +9,8 @@
 # Un Synodic Month equivale a 29 días con 12 horas, con 44 minutos y 3 segundos.
 library(lubridate)
 library(readxl)
+library(stringr)
+library(dplyr)
 dia_SM <- 29
 horas_SM <- 12
 minutos_SM <- 44
@@ -26,17 +28,39 @@ Fecha_final
 ## Parte 2: Agrupaciones y operaciones con fechas
 
 data <- readxl::read_xlsx("data.xlsx")
-primero <- (as_date(as.numeric(data[1,1]), origin = dmy("30/12/1899")))
-day(primero)
-as.Date.numeric(43020, origin = dmy("30/12/1899"))
-# 1.	¿En qué meses existe una mayor cantidad de llamadas por código?
+#Para las fechas Creación
+fechas_num <- data
+fechas_dis <- data
+fechas_num$`Fecha Creación` <- format((as_date(as.numeric(fechas_num$`Fecha Creación`), origin = dmy("30/12/1899"))), "%d-%m-%Y")
+fechas_num$`Fecha Creación` <- dmy(fechas_num$`Fecha Creación`)
 
+fechas_dis$`Fecha Creación` <- dmy(fechas_dis$`Fecha Creación`)
 
-# 2.	¿Qué día de la semana es el más ocupado?
-# 3.	¿Qué mes es el más ocupado?
-# 4.	¿Existe una concentración o estacionalidad en la cantidad de llamadas?
-# 5.	¿Cuántos minutos dura la llamada promedio?
-# 6.	Realice una tabla de frecuencias con el tiempo de llamada.
+fechas_dis <- fechas_dis[!is.na(fechas_dis$`Fecha Creación`),]
+
+fechas_num <- fechas_num[!is.na(fechas_num$`Fecha Creación`),]
+
+data <- bind_rows(fechas_dis, fechas_num)
+
+#Para las fechas final
+fechas_num <- data
+fechas_dis <- data
+fechas_num$`Fecha Final` <- format((as_date(as.numeric(fechas_num$`Fecha Final`), origin = dmy("30/12/1899"))), "%d-%m-%Y")
+fechas_num$`Fecha Final` <- dmy(fechas_num$`Fecha Final`)
+
+fechas_dis$`Fecha Final` <- dmy(fechas_dis$`Fecha Final`)
+
+fechas_dis <- fechas_dis[!is.na(fechas_dis$`Fecha Final`),]
+
+fechas_num <- fechas_num[!is.na(fechas_num$`Fecha Final`),]
+data <- bind_rows(fechas_dis, fechas_num)
+
+# 1.	¿En qué meses existe una mayor cantidad de llamadas por código? agrupar por mes y código. summarize = n()
+# 2.	¿Qué día de la semana es el más ocupado? agrupar por weekdays, summarize
+# 3.	¿Qué mes es el más ocupado? - agrupar por mes, summarize
+# 4.	¿Existe una concentración o estacionalidad en la cantidad de llamadas? - lo mismo del mes, solo que dias
+# 5.	¿Cuántos minutos dura la llamada promedio? 14.algo. mean(difftime(horafinal, horainicio, unit = minutos))
+# 6.	Realice una tabla de frecuencias con el tiempo de llamada. mutatua o grafica del difftime
 
   
   
